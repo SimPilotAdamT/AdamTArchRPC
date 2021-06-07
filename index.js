2690b780ccada869de298dd4d1b0a273fdab5225
SimPilotAdamT <adam_tazul@outlook.com>
2021-06-07 12:15:44 +0100

    Polishing the READM

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
+const os = require('os');
 const RPC = require('discord-rpc');
-const client = new RPC.Client({
-    transport: 'ipc'
-});
+const {exec} = require("child_process");
+const client = new RPC.Client({transport: 'ipc'});
 
-client.on('ready', () => {
-    client.request('SET_ACTIVITY', {
-        pid: process.pid,
-        activity: {
-            details: "Kernel: Linux 5.12.8-arch1-1",
-            state: "WM: XMonad",
-            timestamps: {
-                start: Date.now()
-            },
-            assets: {
-                large_image: "arch-logo", // large image key from developer portal > rich presence > art assets
-                large_text: "Arch Linux"
-		//small_image: "xmonad-logo" // small image key from developer portal > rich presence > art assets
-		//small_test: "Xmonad"
-            },
-            buttons: [
-                {label: "Learn more about Arch Linux", url: "https://archlinux.org/"},
-		{label: "Learn more about XMonad", url: "https://xmonad.org/"}
-            ]
-        }
+exec("stat -c %Z /proc/", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    client.on('ready', () => {
+        client.request('SET_ACTIVITY', {
+            pid: process.pid,
+            activity: {
+                details: "Kernel: " + os.type() + " " + os.release(),
+                state: "WM: XMonad",
+                timestamps: {
+                    start: parseInt(`${stdout}`)
+                },
+                assets: {
+                    large_image: "arch-logo", // large image key from developer portal > rich presence > art assets
+                    large_text: "Arch Linux"
+                },
+                buttons: [{
+                        label: "Learn more about Arch Linux",
+                        url: "https://archlinux.org/"
+                    },
+                    {
+                        label: "Learn more about Xmonad",
+                        url: "https://xmonad.org/"
+                    }
+                    /*{
+                    	label: "Learn more about GNOME",
+                    	url: "https://www.gnome.org/"
+                    }*/
+                    /*{
+                    	label: "Learn more about KDE Plasma",
+                    	url: "https://kde.org/"
+                    }
+                    */
+                ]
+            }
+        });
     });
-});
 
-client.login({
-    clientId: '848998426317946881', // put the client id from the dev portal here
-    clientSecret: 'DjgpoVfYiazwxTRxoQDtp7STJMI9BEXY' // put the client secret from the dev portal here
+    client.login({
+        clientId: '848998426317946881', // put the client id from the dev portal here
+        clientSecret: 'DjgpoVfYiazwxTRxoQDtp7STJMI9BEXY' // put the client secret from the dev portal here
+    });
+    console.log("Rich Presence applied!");
 });
